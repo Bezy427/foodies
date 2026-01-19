51b4cd544e159d8fce8c1a2280e75c401b68e6e5
Bezy427 <rusamubezel@gmail.com>
2026-01-19 18:53:10 +0200

    Initial Commit

---
+++ adminpanel/src/Components/ComponentA.jsx
@@ -0,0 +1,19 @@
+import React, {createContext, useState} from 'react'
+import ComponentB from "./ComponentB.jsx";
+
+export const UserContext = createContext()
+
+const ComponentA = () => {
+    const [user, setUser] = useState("Bezy");
+
+    return (
+        <div className="box">
+            <h1>ComponentA</h1>
+            <h2>{`Hello ${user}`}</h2>
+            <UserContext.Provider value={user}>
+                <ComponentB user={user}/>
+            </UserContext.Provider>
+        </div>
+    )
+}
+export default ComponentA
